be2d142658ef146c4d0b280983f7b28ea627c5d6
Michael <100001154+MichaelGitHub80@users.noreply.github.com>
2026-05-31 15:35:37 +0200

    Create mod_adder_vbe.py

---
+++ src/mod_adder_vbe.py
@@ -0,0 +1,58 @@
+from qiskit import QuantumRegister, QuantumCircuit
+from qiskit.circuit.library import VBERippleCarryAdder
+
+def mod_adder_vbe(n, N_val):
+
+    a = QuantumRegister(n, 'a') 
+    b = QuantumRegister(n, 'b')
+    c = QuantumRegister(n-1, 'c')
+    N = QuantumRegister(n, 'N')
+    t = QuantumRegister(1, 't')
+    
+    qc = QuantumCircuit(a, b, c, N, t)
+    
+    adder = VBERippleCarryAdder(n, kind="fixed").to_gate(label="mod ADD")
+    sub = adder.inverse()
+    
+    qc.append(adder, a[:] + b[:] + c[:])
+    
+    # swap registers: |a>|N> -> |N>|a>
+    for i in range(n):
+        qc.swap(a[i], N[i])
+        
+    # subtract N
+    qc.append(sub, a[:] + b[:] + c[:])
+    
+    # copy subtraction overflow/borrow bit into t
+    qc.x(b[n-1])
+    qc.cx(b[n-1], t[0])
+    qc.x(b[n-1])
+    
+    # if a + b <= N -> MSB = 1, add back N
+    # if a + b > N -> MSB = 0, keep result, add back 0
+    for i in range(n):
+        if (N_val >> i) & 1:
+            qc.cx(t[0], a[i])
+            
+    # add back N or 0 depending on state of t
+    qc.append(adder, a[:] + b[:] + c[:])
+     
+    # uncompute
+    for i in range(n):
+       if (N_val >> i) & 1:
+           qc.cx(t[0], a[i])     
+     
+    # swap registers: |N>|a> -> |a>|N>
+    for i in range(n):
+        qc.swap(a[i], N[i])
+        
+    # subtract original a
+    qc.append(sub, a[:] + b[:] + c[:])
+    
+    # uncompute t
+    qc.cx(b[n-1], t[0])
+    
+    # uncompute subtraction
+    qc.append(adder, a[:] + b[:] + c[:])
+    
+    return qc.to_gate(label=f"mod_adder_vbe_{N_val}")
